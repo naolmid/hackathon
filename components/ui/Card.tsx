fc3a5c47302bf18ad1a5e609d7bf092aa5e35983
naolmid <naolmideksa@gmail.com>
2026-01-03 05:39:15 +0300

    Fix Card onClick type

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 interface CardProps {
   children: React.ReactNode;
   className?: string;
-  onClick?: () => void;
+  onClick?: (e?: React.MouseEvent) => void;
   hoverable?: boolean;
 }
 
